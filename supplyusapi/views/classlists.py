9a8b936f3754b83d1735ffa14bc348e29246bd6f
Your Github User Name <david.lav.williams@gmail.com>
2021-03-08 14:03:27 -0600

    supply items has a filter by type method

--- supplyusapi/views/classlists.py
+++ supplyusapi/views/classlists.py
@@ -41,7 +41,7 @@ class ClassLists(ViewSet):
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+    
 class ClassListSerializer(serializers.ModelSerializer):
     class Meta:
         model= ClassList
