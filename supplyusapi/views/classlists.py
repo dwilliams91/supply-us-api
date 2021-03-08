7801e6454d1c98a0069e9b323e48301ffc385a5a
Your Github User Name <david.lav.williams@gmail.com>
2021-03-08 10:14:16 -0600

    the viewset for classLists has a get all method returning every class

--- supplyusapi/views/classlists.py
+++ supplyusapi/views/classlists.py
@@ -5,8 +5,17 @@ from rest_framework import status
 from django.http import HttpResponseServerError
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from supplyusapi.models import ClassList
 
 
 class ClassLists(ViewSet):
     def list(self, request):
-        author= User.objects.get(user=request.auth.user)
+        classlists=ClassList.objects.all()
+        print(classlists)
+        serializer=ClassListSerializer(classlists, many=True, context={'request':request})
+        return Response(serializer.data)
+        
+class ClassListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= ClassList
+        fields=('id','class_name')
