6bc95427e7411f3c93e0b1e322a5bf139ea778d2
Your Github User Name <david.lav.williams@gmail.com>
2021-03-10 10:36:01 -0600

    the post method is getting routed correct. need to check if the data is coming in correctly

--- supplyusapi/views/supplyitems.py
+++ supplyusapi/views/supplyitems.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 
 from django.http import HttpResponseServerError
 from django.core.exceptions import ValidationError
-from supplyusapi.models import SupplyItem, SupplyType, ClassListSupplyItem, PackageType
+from supplyusapi.models import SupplyItem, SupplyType, ClassListSupplyItem, PackageType, ClassList
 
 class SupplyItems(ViewSet):
     def list(self, request):
@@ -24,12 +24,31 @@ class SupplyItems(ViewSet):
         serializer=SupplyItemsSerializer(filtered_items, many=True, context={'request':request})
         return Response(serializer.data)
 
-    @action(methods=['get'], detail=True)
+    @action(methods=['get', 'post'], detail=True)
     def classListSupplyItem(self, request, pk=None):
         if request.method=="GET":
             list_of_my_class_items=ClassListSupplyItem.objects.filter(class_list=pk)
             serializer=ClassListSupplyItemSerializer(list_of_my_class_items, many=True, context={'request':request})
             return Response(serializer.data)
+        if request.method=="POST":
+
+            new_item=ClassListSupplyItem()
+
+            related_Class=ClassList.objects.get(pk=request.data["classListId"])
+            related_supply_item=SupplyItem.objects.get(pk=request.data["supplyItemId"])
+            related_PackageType=PackageType.objects.get(pk=request.data["packaging"])
+
+            new_item.number=request.data["number"]
+            new_item.class_list=related_Class
+            new_item.supply_item=related_supply_item
+            new_item.package_type= related_PackageType
+            
+            new_item.description=request.data["description"]
+            serializer=ClassListSupplyItemSerializer(new_item, many=False, context={'request':request})
+            return Response(status=status.HTTP_201_CREATED)
+ 
+
+    
 
         
 class SupplyItemsSerializer(serializers.ModelSerializer):
