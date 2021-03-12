e8b1f7853b912b2a72acd3ca6c9a8f4fcf16a35b
Your Github User Name <david.lav.williams@gmail.com>
2021-03-12 15:12:23 -0600

    is_active_type is now on model

--- supplyusapi/models/package_types.py
+++ supplyusapi/models/package_types.py
@@ -3,3 +3,4 @@ from django.db import models
 class PackageType(models.Model):
     supply_item=models.ForeignKey("SupplyItem", on_delete=models.CASCADE)
     type=models.CharField(max_length=50)
+    is_active_type=models.BooleanField()
