8a8309e867609824da52364f2441d23a9ec22c3a
Your Github User Name <david.lav.williams@gmail.com>
2021-03-22 09:37:41 -0500

    get unapproved teachers works

--- supplyusapi/views/admin.py
+++ supplyusapi/views/admin.py
@@ -10,15 +10,14 @@ from supplyusapi.models import ClassList, UserClass
 from rest_framework.decorators import action
 
 class Admins(ViewSet):
-
-    @action(methods=['Get'],detail=True)
+    @action(methods=['Get'],detail=False)
     def getPendingTeachers(self,request,pk=None):
         current_user=User.objects.get(auth_token=request.auth)
         if current_user.is_superuser==True:
-            pending_teachers=User.objects.get(is_active=False)
+            pending_teachers=User.objects.filter(is_active=0)
         serializer=UserSerlializer(pending_teachers, many=True, context={'request':request})
         return Response(serializer.data)
 class UserSerlializer(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields=('email')
+        fields=('last_name','email')
